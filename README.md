890a306e19283d397ecf828cd42c1aaf37bf102b
Austin Rivas <arivas@pushhere.com>
2017-11-17 14:33:05 -0500

    working local configuration

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # node-docker-multi-env-test
+
+
+Inspiration : https://staxmanade.com/2016/07/run-multiple-docker-environments--qa--beta--prod--from-the-same-docker-compose-file-/
---
+++ app/server.js
@@ -0,0 +1,15 @@
+let http = require('http');
+
+let server = http.createServer(function(req, res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end(`
+Hello World!
+
+VIRTUAL_HOST: ${process.env.VIRTUAL_HOST}
+NODE_ENV: ${process.env.NODE_ENV}
+PORT: ${process.env.PORT}
+
+`.split('/n').join('<br>'));
+});
+
+server.listen(80);
