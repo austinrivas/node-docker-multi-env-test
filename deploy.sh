001ec8a73e8876eff44ed75b333fcd48fc31fca6
Austin Rivas <arivas@pushhere.com>
2017-11-17 16:35:43 -0500

    removed DOTOKEN FROM VC

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DOTOKEN=6457ffa0c3346c86c6072c295f18e452911bbea513dbc652bebdb227ed81c59e
-DIGITALOCEAN_IMAGE="ubuntu-16-04-x64"
-
-docker-machine create --driver digitalocean --digitalocean-access-token $DOTOKEN docker-multi-environment
