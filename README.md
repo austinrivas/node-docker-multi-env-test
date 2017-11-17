91536264e38db16037c4545a84673c701a89f78e
Austin Rivas <arivas@pushhere.com>
2017-11-17 16:31:36 -0500

    added local.startup.sh local.shutdown.sh local.cleanup.sh

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 
 Inspiration : https://staxmanade.com/2016/07/run-multiple-docker-environments--qa--beta--prod--from-the-same-docker-compose-file-/
+DO Docs: https://docs.docker.com/machine/examples/ocean/#step-2-generate-a-personal-access-token
+
+Shell access to digitalocean instance
+`eval $(docker-machine env docker-multi-environment)`
---
+++ local.cleanup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# qa
+export NODE_ENV=qa
+export PORT=8001
+export VIRTUAL_HOST=$NODE_ENV.$BASE_SITE
+export CONTAINER_NAME="${NODE_ENV}test_web_1"
+export IMAGE_NAME="${NODE_ENV}test_web"
+docker rm ${CONTAINER_NAME}
+docker rmi ${IMAGE_NAME}
+
+
+# prod
+export NODE_ENV=production
+export PORT=8003
+export VIRTUAL_HOST=$BASE_SITE
+export CONTAINER_NAME="${NODE_ENV}test_web_1"
+export IMAGE_NAME="${NODE_ENV}test_web"
+docker rm ${CONTAINER_NAME}
+docker rmi ${IMAGE_NAME}
+
+# nginx-proxy
+docker network disconnect service-tier nginx-proxy
+docker rm nginx-proxy
+docker rmi jwilder/nginx-proxy
+
+# nginx-proxy network
+docker network rm service-tier
+
+# node
+docker rmi node
+
